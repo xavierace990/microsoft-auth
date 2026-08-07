34da2d65b0a2ebc0c0002535f495c9d174ff1f24
xavierace990 <xavierparzival@gmail.com>
2026-08-07 13:44:40 -0700

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s2i9i&yqr-)eb#oziv$5*-hr03s0rp-he$0_@8arv6)g$ba1n7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['aethersy.vercel.app']
+ALLOWED_HOSTS = ['nexlity.vercel.app']
 
 
 # Application definition
